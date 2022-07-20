aef9810237123a9f65c5afadd1df2a7bfdb9b298
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-07-20 18:31:09 +0300

    preper user  store  -done

---
+++ blog/src/redux/reducers/users/users.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const users = createSlice({
+    name: "users",
+    initialState: {
+        users: [],
+        },
+    reducers: {
+        setUsers: (state, action) => {
+            state.users = action.payload;
+        }
+    }
+})
+
+export const {setUsers} =   users.actions; 
+
+export default users.reducer;
